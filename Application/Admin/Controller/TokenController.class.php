fdf95ea5959fdd1ff6fefdc8dee339e84b369217
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-15 18:10:04 +0800

    令牌控制器

---
+++ Application/Admin/Controller/TokenController.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+
+class TokenController extends Controller{
+    public function index(){
+        $this->display();
+    }
+
+    public function add_token(){
+        $this->display();
+    }
+}
+?>
