80bc6ee2d0a862b8edb457e4c70c9459cd2c62b2
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-18 15:01:59 +0800

    首页

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -3,9 +3,17 @@ namespace Admin\Controller;
 
 use Think\Controller;
 
-class IndexController extends Controller{
+class IndexController extends CommonController{
+    //公共方法
+    public function _initialize(){
+        $this->check_login();//检查登录
+    }
+    //首页
     public function index(){
+        $admin = $_SESSION['admin'];
+        $this->assign('admin',$admin);
         $this->display();
     }
+
 
 }
