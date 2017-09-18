a784d9e4af2e4a32b447d4ae36ef5de3d72211c5
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-18 17:07:28 +0800

    新闻管理

--- Application/Admin/Controller/NewsController.class.php
+++ Application/Admin/Controller/NewsController.class.php
@@ -18,13 +18,66 @@ class NewsController extends Controller{
     public function update_news(){
         $this->display();
     }
+
+
+
     //分类列表
     public function news_cate(){
+        $a = M('news_cate')->where('pid = 0')->select();
+        foreach ($a as $k => $v){
+            for ($i = 0;$i < count($a);$i++){
+                $a[$i]['cate'] = M('news_cate')->where('pid = '.$a[$i]['cate_id'])->select();
+            }
+        }
+       $this->assign('cate',$a);
         $this->display();
     }
     //添加分类
     public function add_cate(){
+        $a = M('news_cate')->where('pid = 0')->select();
+
+        $this->assign('cate_p',$a);
+
         $this->display();
+    }
+    public function save_cate(){
+        if ($_POST){
+            $res = M('news_cate')->data($_POST)->add();
+            if ($res){
+                $this->success('添加成功',U('News/news_cate'));
+            }else{
+                $this->error('添加失败，请稍后重试！');
+            }
+        }
+    }
+    //删除分类
+    public function destroy_cate()
+    {
+        $id = $_GET['id'];
+        $response = M('news')->where('cate_id = '.$id)->select();
+
+        if (empty($response)){
+            $res = M('news_cate')->where('cate_id = '.$id)->delete();
+            if ($res){
+                $data = [
+                    'status' => 1,
+                    'msg'    => '删除成功'
+                ];
+                echo json_encode($data);
+            }else{
+                $data = [
+                    'status' => 0,
+                    'msg'    => '删除失败，请稍后重试！'
+                ];
+                echo json_encode($data);
+            }
+        }else{
+            $data = [
+                'status' => 2,
+                'msg'    => '此分类下含有文章，请删除文章后再试！'
+            ];
+            echo json_encode($data);
+        }
     }
 }
 ?>
