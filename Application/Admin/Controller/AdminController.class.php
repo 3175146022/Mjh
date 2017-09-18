42e40eecc44c70c099a9d059fad4d9e436be8de2
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-18 09:33:24 +0800

    后台管理员管理

---
+++ Application/Admin/Controller/AdminController.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+
+class AdminController extends Controller{
+    public function index(){
+        $this->display();
+    }
+    public function add_user(){
+        $this->display();
+    }
+    public function update_user(){
+        $this->display();
+    }
+}
+
+?>
