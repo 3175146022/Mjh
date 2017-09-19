79420fce6257979adcd9d178186f583ae7422da0
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-19 10:26:26 +0800

    令牌管理

--- Application/Admin/Controller/TokenController.class.php
+++ Application/Admin/Controller/TokenController.class.php
@@ -25,12 +25,52 @@ class TokenController extends Controller{
                     $this->success('添加成功！');
                 }
             }
+        }else{
+            $this->display();
         }
-        $this->display();
     }
     //修改令牌
     public function update_token(){
-        $this->display();
+        //关闭表单令牌
+        C('TOKEN_ON',false);
+        if(IS_POST){
+            $post =array(
+                'token_id' => I('post.token_id'),
+            );
+            $verify = D('Token');
+            if(!$verify->create()){
+                $this->error($verify->getError());
+            }else{
+                $temp['tok_num'] = $_POST['tok_num'];
+                $temp['tok_level'] = $_POST['tok_level'];
+                $result = M('Token')->where(array('token_id'=>$post['token_id']))->save($temp);
+                if($result){
+                    $this->success('修改成功！',U('Token/index'));
+                }
+            }
+        }else{
+            $id = I('get.id');
+            $data = M('Token')->where(array('token_id'=>$id))->find();
+            $this->assign('data',$data);
+            $this->display();
+        }
+    }
+    public function delete_token(){
+        $id = I('get.id');
+        $result = M('Token')->where(array('token_id'=>$id))->delete();
+        if ($result){
+            $data = [
+                'status' => 1,
+                'msg'    => '删除成功'
+            ];
+            echo json_encode($data);
+        }else{
+            $data = [
+                'status' => 0,
+                'msg'    => '删除失败，请稍后重试！'
+            ];
+            echo json_encode($data);
+        }
     }
 }
 ?>
--- Application/Admin/Model/TokenModel.class.php
+++ Application/Admin/Model/TokenModel.class.php
@@ -6,6 +6,7 @@ class TokenModel extends Model{
     //自动验证
     protected $_validate = array(
         array('tok_num','require','请输入令牌编码！'), //默认情况下用正则进行验证
+        array('tok_num','','令牌已存在！',0,'unique',3), // 在新增的时候验证admin_name字段是否唯一
     );
 }
 ?>
