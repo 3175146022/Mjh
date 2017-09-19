3604da81cdd06a4756707ef3338064bf99f9dc03
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-19 17:57:09 +0800

    公共模块

--- Application/Admin/Controller/CommonController.class.php
+++ Application/Admin/Controller/CommonController.class.php
@@ -9,5 +9,17 @@ class CommonController extends Controller{
             $this->redirect('Login/index');
         }
     }
+    //上传图片
+    public function upload(){
+        //添加图片
+        $upload = new \Think\Upload();                              // 实例化上传类
+        $upload->maxSize = 0;                                // 设置附件上传大小
+        $upload->exts = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+        $upload->rootPath = './Uploads/';                      // 设置附件上传根目录
+        $upload->savePath = 'activity/';                       // 设置附件上传（子）目录
+        // 上传文件
+        $info = $upload->upload();
+        return $info;
+    }
 }
 ?>
