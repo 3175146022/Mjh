e2cb73f384ad6f172c88b51a86d29eb06ab28dbd
Wan Zhongwen <wanzhongwen233@outlook.com>
2017-09-22 18:24:19 +0800

    个人信息 我的积分

--- Application/Home/Controller/PointsController.class.php
+++ Application/Home/Controller/PointsController.class.php
@@ -3,10 +3,9 @@ namespace Home\Controller;
 
 use Think\Controller;
 
-class UserController extends Controller{
+class PointsController extends Controller{
     public function index(){
 
         $this->display();//页面赋值
     }
-
 }
