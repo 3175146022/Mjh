31a8bb3fa5f20420203287260d2d313b2ea08ce4
Wan Zhongwen <wanzhongwen233@outlook.com>
2017-09-22 18:37:55 +0800

    我的江湖

---
+++ Application/Home/Controller/MyactivityController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Home\Controller;
+
+use Think\Controller;
+
+class MyactivityController extends Controller{
+    public function index(){
+
+        $this->display();//页面赋值
+    }
+}
