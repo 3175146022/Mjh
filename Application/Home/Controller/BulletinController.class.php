3b755b6695ef02570f99a80a0a5c34509b56bda7
Wan Zhongwen <wanzhongwen233@outlook.com>
2017-09-22 17:13:39 +0800

    江湖须知

--- Application/Home/Controller/BulletinController.class.php
+++ Application/Home/Controller/BulletinController.class.php
@@ -3,14 +3,9 @@ namespace Home\Controller;
 
 use Think\Controller;
 
-class StoryController extends Controller{
+class BulletinController extends Controller{
     public function index(){
 
         $this->display();//页面赋值
-    }
-
-    public function show()
-    {
-        $this->display();
     }
 }
