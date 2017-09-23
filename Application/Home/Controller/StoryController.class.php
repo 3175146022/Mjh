e190b562db9e97d59c96ad65d23e7f06b71a6f22
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-23 15:38:34 +0800

    江湖故事

--- Application/Home/Controller/StoryController.class.php
+++ Application/Home/Controller/StoryController.class.php
@@ -4,13 +4,17 @@ namespace Home\Controller;
 use Think\Controller;
 
 class StoryController extends Controller{
+    //江湖故事列表
     public function index(){
-
+        $list = M('news')->where(array('cate_name'=>'江湖故事'))->join('news_cate on news_cate.cate_id = news.cate_id')->select();
+        $this->assign('list',$list);
         $this->display();//页面赋值
     }
-
-    public function show()
-    {
+    //江湖故事详情
+    public function show(){
+        $id = I('get.id');
+        $data = M('News')->where(array('news_id'=>$id))->field('content')->find();
+        $this->assign('data',$data);
         $this->display();
     }
 }
