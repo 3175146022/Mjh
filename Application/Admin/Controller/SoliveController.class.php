2ce3e73461a2daf7bc21a3fbcab75e1719cda384
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-23 17:02:06 +0800

    直播后台

--- Application/Admin/Controller/SoliveController.class.php
+++ Application/Admin/Controller/SoliveController.class.php
@@ -2,12 +2,14 @@
 namespace Admin\Controller;
 use Think\Controller;
 
-class SoliveController extends Controller{
+class SoliveController extends CommonController{
+    //直播列表
     public function index(){
         $data = M('Solive')->select();
         $this->assign('data',$data);
         $this->display();
     }
+    //添加直播
     public function add_solive(){
         //关闭表单令牌
         C('TOKEN_ON',false);
@@ -17,17 +19,30 @@ class SoliveController extends Controller{
                 $this->error($verify->getError());
             }else {
                 $_POST['add_time'] = NOW_TIME;
-                $res = M('Solive')->add($_POST);
-                if($res){
-                    $this->success('添加成功',U('Solive/add_solive'));
+                $upload = new \Think\Upload();// 实例化上传类
+                $upload->maxSize   =     0 ;// 设置附件上传大小
+                $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+                $upload->rootPath  =     './Uploads/'; // 设置附件上传根目录
+                $upload->savePath  =     'solive/'; // 设置附件上传（子）目录
+                // 上传文件
+                $info   =   $upload->upload();
+                if(!$info['img']){
+                    echo "<script>alert('请添加图片！');window.history.go(-1)</script>";
                 }else{
-                    echo "<script>alert('添加失败！');window.history.go(-1)</script>";
+                    $_POST['img'] = 'Uploads/'.$info['img']['savepath'].$info['img']['savename'];
+                    $res = M('Solive')->add($_POST);
+                    if($res){
+                        $this->success('添加成功',U('Solive/add_solive'));
+                    }else{
+                        echo "<script>alert('添加失败！');window.history.go(-1)</script>";
+                    }
                 }
             }
         }else{
             $this->display();
         }
     }
+    //修改直播
     public function update_solive(){
         //关闭表单令牌
         C('TOKEN_ON',false);
@@ -36,7 +51,28 @@ class SoliveController extends Controller{
             if(!$verify->create()){
                 $this->error($verify->getError());
             }else {
-                $res = M('Solive')->where(array('solive_id'=>$_POST['solive_id']))->save($_POST);
+                $upload = new \Think\Upload();// 实例化上传类
+                $upload->maxSize   =     0 ;// 设置附件上传大小
+                $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+                $upload->rootPath  =     './Uploads/'; // 设置附件上传根目录
+                $upload->savePath  =     'solive/'; // 设置附件上传（子）目录
+                // 上传文件
+                $info   =   $upload->upload();
+                $post = array(
+                    'so_title' => $_POST['so_title'],
+                    'so_detail' => $_POST['so_detail'],
+                    'so_author' => $_POST['so_author'],
+                    'so_link' => $_POST['so_link'],
+                );
+                if($info['img']) {
+                    //获取原来的图片地址
+                    $pic = M('Solive')->field('img')->where(array('news_id' => $_POST['solive_id']))->find();
+                    //删除原来的图片
+                    unlink($pic['img']);
+                    $img_path = 'Uploads/' . $info['img']['savepath'] . $info['img']['savename'];
+                    $post['img'] = $img_path;
+                }
+                $res = M('Solive')->where(array('solive_id'=>$_POST['solive_id']))->save($post);
                 if($res){
                     $this->success('修改成功',U('Solive/index'));
                 }else{
@@ -50,8 +86,11 @@ class SoliveController extends Controller{
             $this->display();
         }
     }
+    //删除直播
     public function delete_solive(){
         $id = I('get.id');
+        $pic = M('Solive')->field('img')->where(array('news_id'=>$id))->find();
+        unlink($pic['img']);
         $result = M('Solive')->where(array('solive_id'=>$id))->delete();
         if ($result){
             $data = [
