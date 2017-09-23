9372f013dc81e43e5503d878c4715598fd3ef9a6
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-23 14:10:53 +0800

    江湖活动

---
+++ Application/Home/Controller/ActivityController.class.php
@@ -0,0 +1,22 @@
+<?php
+namespace Home\Controller;
+
+use Think\Controller;
+
+class ActivityController extends Controller{
+    //江湖活动列表
+    public function index(){
+
+        $this->display();//页面赋值
+    }
+    //江湖活动更多
+    public function more(){
+
+        $this->display();//页面赋值
+    }
+    //江湖活动详情
+    public function show(){
+
+        $this->display();//页面赋值
+    }
+}
