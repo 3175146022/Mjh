076fda67f2a9855af5c38f7650af1a302abf5c80
Wan Zhongwen <wanzhongwen233@outlook.com>
2017-09-23 09:35:03 +0800

    我的江湖

--- Application/Admin/Model/NewsModel.class.php
+++ Application/Admin/Model/NewsModel.class.php
@@ -5,6 +5,7 @@ use Think\Model;
 class NewsModel extends Model{
     protected $_validate = array(
         array('title','require','请输入新闻标题！',0),
+        array('introduction','require','请输入简介！',0),
         array('cate_id','require','请输入封面详解！',0),
         array('content','require','请输入内容！',0),
     );
