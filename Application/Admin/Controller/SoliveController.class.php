fac8c226887fc882500ecbbbb6813c749f234d0c
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-23 16:20:47 +0800

    后台直播

--- Application/Admin/Controller/SoliveController.class.php
+++ Application/Admin/Controller/SoliveController.class.php
@@ -11,19 +11,13 @@ class SoliveController extends Controller{
     public function add_solive(){
         //关闭表单令牌
         C('TOKEN_ON',false);
-        $post = array(
-            'so_title' => I('post.so_title'),
-            'so_detail' => I('post.so_detail'),
-            'so_author' => I('post.so_author'),
-            'so_link' => I('post.so_link'),
-            'add_time' => NOW_TIME,
-        );
         if(IS_POST){
             $verify = D('Solive');
             if(!$verify->create()){
                 $this->error($verify->getError());
             }else {
-                $res = M('Solive')->add($post);
+                $_POST['add_time'] = NOW_TIME;
+                $res = M('Solive')->add($_POST);
                 if($res){
                     $this->success('添加成功',U('Solive/add_solive'));
                 }else{
@@ -37,19 +31,12 @@ class SoliveController extends Controller{
     public function update_solive(){
         //关闭表单令牌
         C('TOKEN_ON',false);
-        $post = array(
-            'solive_id' => I('post.solive_id'),
-            'so_title' => I('post.so_title'),
-            'so_detail' => I('post.so_detail'),
-            'so_author' => I('post.so_author'),
-            'so_link' => I('post.so_link'),
-        );
         if(IS_POST){
             $verify = D('Solive');
             if(!$verify->create()){
                 $this->error($verify->getError());
             }else {
-                $res = M('Solive')->where(array('solive_id'=>$post['solive_id']))->save($post);
+                $res = M('Solive')->where(array('solive_id'=>$_POST['solive_id']))->save($_POST);
                 if($res){
                     $this->success('修改成功',U('Solive/index'));
                 }else{
