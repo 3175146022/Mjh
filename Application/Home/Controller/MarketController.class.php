5bf63c0a255b47734ab9e49346fdd9611a3881a5
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-23 15:30:45 +0800

    江湖推介

--- Application/Home/Controller/MarketController.class.php
+++ Application/Home/Controller/MarketController.class.php
@@ -6,12 +6,15 @@ use Think\Controller;
 class MarketController extends Controller{
     //江湖直播列表
     public function index(){
-
+        $list = M('news')->where(array('cate_name'=>'江湖推介'))->join('news_cate on news_cate.cate_id = news.cate_id')->select();
+        $this->assign('list',$list);
         $this->display();//页面赋值
     }
     //江湖直播详情
     public function show(){
-
+        $id = I('get.id');
+        $data = M('News')->where(array('news_id'=>$id))->field('content')->find();
+        $this->assign('data',$data);
         $this->display();//页面赋值
     }
 }
