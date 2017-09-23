58beb126c68fa107e34a317677a383cc7feeccef
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-23 14:23:25 +0800

    江湖推介

---
+++ Application/Home/Controller/MarketController.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Home\Controller;
+
+use Think\Controller;
+
+class MarketController extends Controller{
+    //江湖直播列表
+    public function index(){
+
+        $this->display();//页面赋值
+    }
+    //江湖直播详情
+    public function show(){
+
+        $this->display();//页面赋值
+    }
+}
