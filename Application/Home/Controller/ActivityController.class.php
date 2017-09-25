c01737be49148514253b05ff12d04eb19e746ba3
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-25 10:28:47 +0800

    江湖活动

--- Application/Home/Controller/ActivityController.class.php
+++ Application/Home/Controller/ActivityController.class.php
@@ -6,17 +6,22 @@ use Think\Controller;
 class ActivityController extends Controller{
     //江湖活动列表
     public function index(){
-
+        $list = M('Activity')->join('activity_cate on activity_cate.act_cate_id = activity.act_cate_id')->select();
+        $this->assign('list',$list);
         $this->display();//页面赋值
     }
     //江湖活动更多
     public function more(){
-
+        $id = I('get.id');
+        $more = M('Activity')->where(array('act_cate_id'=>$id))->select();
+        $this->assign('more',$more);
         $this->display();//页面赋值
     }
     //江湖活动详情
     public function show(){
-
+        $id = I('get.id');
+        $data = M('Activity')->where(array('activity_id'=>$id))->find();
+        $this->assign('data',$data);
         $this->display();//页面赋值
     }
 }
