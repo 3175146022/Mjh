b126c74a7b76a78516e5ba916836f49595d6e304
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-25 13:21:38 +0800

    前端江湖故事

--- Application/Home/Controller/StoryController.class.php
+++ Application/Home/Controller/StoryController.class.php
@@ -13,7 +13,7 @@ class StoryController extends Controller{
     //江湖故事详情
     public function show(){
         $id = I('get.id');
-        $data = M('News')->where(array('news_id'=>$id))->field('content')->find();
+        $data = M('News')->where(array('news_id'=>$id))->field('title,content')->find();
         $this->assign('data',$data);
         $this->display();
     }
