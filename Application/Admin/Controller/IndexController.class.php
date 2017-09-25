babe189d3813bad2befe663f162bbbb1930c11af
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-25 15:11:29 +0800

    后台首页

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -14,6 +14,10 @@ class IndexController extends CommonController{
         $this->assign('admin',$admin);
         $this->display();
     }
-
+    public function index_v1(){
+        $list = M('Activity')->order(array('add_time'=>'desc'))->limit(10)->select();
+        $this->assign('list',$list);
+        $this->display();
+    }
 
 }
