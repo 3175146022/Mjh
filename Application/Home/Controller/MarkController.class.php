6678c50fccb55ac11ea441e6d46ccb31f5ee39f0
3175146022 <31603692+3175146022@users.noreply.github.com>
2017-09-25 16:55:02 +0800

    签到

--- Application/Home/Controller/MarkController.class.php
+++ Application/Home/Controller/MarkController.class.php
@@ -4,6 +4,8 @@ use Think\Controller;
 
 class MarkController extends Controller{
     public function index(){
+        $data = M('Genera')->order('add_time desc')->limit(1)->find();
+        $this->assign('data',$data);
         $this->display();
     }
 }
