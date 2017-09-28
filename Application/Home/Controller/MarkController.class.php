15b09a6ddfde5cbd0751cb74268bca0049cb90e7
Administrator <lxlwanwan@163.com>
2017-09-28 10:28:11 +0800

    收藏 _1

--- Application/Home/Controller/MarkController.class.php
+++ Application/Home/Controller/MarkController.class.php
@@ -9,6 +9,8 @@ class MarkController extends CommonController{
 
     public function index(){
         $data = M('Genera')->order('add_time desc')->limit(1)->find();
+        $keep = M('collect')->where(array('collect'=>$data['id'],'cate'=>4,'user_id'=>$_SESSION['user_id']))->find();
+        $this->assign('keep',$keep);
         $this->assign('data',$data);
         $this->display();
     }
