1f0d5e9d05db92b02a5d20b78fd0a90a4bd430e3
Wan Zhongwen <wanzhongwen233@outlook.com>
2017-09-29 21:15:49 +0800

    积分 声望 签到

--- Application/Home/Controller/PointsController.class.php
+++ Application/Home/Controller/PointsController.class.php
@@ -8,9 +8,42 @@ class PointsController extends CommonController{
         parent::__construct();
     }
     public function index(){
+        $token_integral = M('user')->where('user_id = '.$_SESSION['user_id'])
+                                   ->join('token ON token.token_id = user.token_id')
+                                   ->find();
+        //var_dump($token_integral);exit;
+        switch ($token_integral['tok_level']){
+            case 1:
+                $token_integral['token_integral'] = 3;
+                $token_integral['token_name'] = '欢喜令';
+                break;
+            case 2:
+                $token_integral['token_integral'] = 6;
+                $token_integral['token_name'] = '义士令';
+                break;
+            case 3:
+                $token_integral['token_integral'] = 18;
+                $token_integral['token_name'] = '英雄令';
+                break;
+            case 4:
+                $token_integral['token_integral'] = 36;
+                $token_integral['token_name'] = '大侠令';
+                break;
+            case 5:
+                $token_integral['token_integral'] = 72;
+                $token_integral['token_name'] = '大谦令';
+                break;
+        }
+        $ti = [
+            'ti' => $token_integral['token_integral'],
+            'tn' => $token_integral['token_name'],
+            'bind_time' => $token_integral['bind_time']
+        ];
+        //var_dump($token_integral);exit;
         $integral = M('user')->where('user_id = '.$_SESSION['user_id'])->field('integral')->find();
-        $mark = M('mark')->where('user_id = '.$_SESSION['user_id'])->order('mark_time desc')->select();
+        $mark = M('mark')->where('user_id = '.$_SESSION['user_id'])->order('mark_time desc')->limit(10)->select();
         //var_dump($mark);exit;
+        $this->assign('token_integral',$ti);
         $this->assign('mark',$mark);
         $this->assign('integral',$integral);
         $this->display();//页面赋值
--- Application/Home/Controller/PopularityController.class.php
+++ Application/Home/Controller/PopularityController.class.php
@@ -10,9 +10,42 @@ class PopularityController extends CommonController{
     }
 
     public function index(){
+        $token_integral = M('user')->where('user_id = '.$_SESSION['user_id'])
+            ->join('token ON token.token_id = user.token_id')
+            ->find();
+        //var_dump($token_integral);exit;
+        switch ($token_integral['tok_level']){
+            case 1:
+                $token_integral['token_reputation'] = 3;
+                $token_integral['token_name'] = '欢喜令';
+                break;
+            case 2:
+                $token_integral['token_reputation'] = 6;
+                $token_integral['token_name'] = '义士令';
+                break;
+            case 3:
+                $token_integral['token_reputation'] = 18;
+                $token_integral['token_name'] = '英雄令';
+                break;
+            case 4:
+                $token_integral['token_reputation'] = 36;
+                $token_integral['token_name'] = '大侠令';
+                break;
+            case 5:
+                $token_integral['token_reputation'] = 72;
+                $token_integral['token_name'] = '大谦令';
+                break;
+        }
+        $tr = [
+            'tr' => $token_integral['token_reputation'],
+            'tn' => $token_integral['token_name'],
+            'bind_time' => $token_integral['bind_time']
+        ];
+
         $reputation = M('user')->where('user_id = '.$_SESSION['user_id'])->field('reputation')->find();
         $mark = M('mark')->where('user_id = '.$_SESSION['user_id'])->order('mark_time desc')->select();
         //var_dump($mark);exit;
+        $this->assign('token_reputation',$tr);
         $this->assign('mark',$mark);
         $this->assign('reputation',$reputation);
         $this->display();//页面赋值
